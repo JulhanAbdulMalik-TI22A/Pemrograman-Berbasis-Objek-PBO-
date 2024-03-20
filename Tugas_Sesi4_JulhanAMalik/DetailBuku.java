95d698b52866765a5e33bdd29c286d19688576b9
Julhan Abdul Malik <114629579+JulhanAbdulMalik-TI22A@users.noreply.github.com>
2024-03-20 21:28:52 +0700

    Update DetailBuku.java

--- Tugas_Sesi4_JulhanAMalik/DetailBuku.java
+++ Tugas_Sesi4_JulhanAMalik/DetailBuku.java
@@ -3,13 +3,24 @@
  * @author julhan
  */
 public class DetailBuku {
+    static int discount(int harga_buku, int diskon){
+        return harga_buku;
+    }
+    
+    static double discount(double harga_buku, double diskon){
+        diskon = diskon * harga_buku;
+        return harga_buku - diskon;
+    }
+    
     public static void main(String[] args) {
         String nama_buku = "Dasar Pemrograman Berbasis Objek";
         String rilis_buku = "20 Maret 2024";
         int harga_buku = 100000;
+        double diskon_buku = discount(harga_buku, 0.4);
         
         PembuatBuku bukuPub = new PembuatBuku(nama_buku, rilis_buku, "Julhan", "JuDul Production", harga_buku);       
         bukuPub.InfoBuku();
         
+        System.out.println("Harga Diskon  : " + diskon_buku);
     }
 }
