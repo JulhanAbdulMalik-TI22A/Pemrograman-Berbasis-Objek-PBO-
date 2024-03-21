718f5583efdfd62a37affc722c6d1489a7b8283a
Julhan Abdul Malik <114629579+JulhanAbdulMalik-TI22A@users.noreply.github.com>
2024-03-21 17:11:59 +0700

    Add files via upload

---
+++ Tugas_Sesi5_JulhanAMalik/person.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author julhan
+ */
+public class person {
+    String name;
+    String address;
+    
+    person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+    
+    
+    
+}
