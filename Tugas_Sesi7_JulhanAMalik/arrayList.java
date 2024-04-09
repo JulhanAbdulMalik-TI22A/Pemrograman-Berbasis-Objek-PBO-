2fbcc9c8c1fdc6b79b864cb4814e6bfed86181af
Julhan Abdul Malik <114629579+JulhanAbdulMalik-TI22A@users.noreply.github.com>
2024-04-09 09:14:59 +0700

    Add files via upload

---
+++ Tugas_Sesi7_JulhanAMalik/arrayList.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Julhan
+ */
+public class arrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println("Isi ArrayList:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
