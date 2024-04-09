5fa5b48bb017439e8c3ba9903702802a9ab2d677
Julhan Abdul Malik <114629579+JulhanAbdulMalik-TI22A@users.noreply.github.com>
2024-04-09 08:30:23 +0700

    Add files via upload

---
+++ Tugas_Sesi6_JulhanAMalik/Laptop.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Julhan
+ */
+public interface Laptop {
+    int MAX_VOL = 100;
+    int MIN_VOL = 0;
+    
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
